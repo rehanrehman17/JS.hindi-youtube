8322f026c268702522ef658138e6e7243b0a7b79
rehanrehman17 <rehanrehman060@gmail.com>
2025-10-17 18:46:45 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version //
+//alert(3+3)// we are using nodejs not browser
+
+console.log(3
+    +3); // code readability should be high
+
+console.log("rehan");
+    
+
+let name = "rehan"
+let age = 21
+let idloggedin = false
+//let state; null
+let state;
+
+// number => 2 to power 53
+// bigint => if your number is really big so we can use bigint like in stock market,trading...etc
+// string => ""
+// null => standalone value jiski value assign nh hti
+// undefined => jis ki koi value provide nh ki humne 
+// symbol => unique (jb bh humm dekhty hain k ye code essy zyada unique hai usky liye humm use krty)
+
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+
