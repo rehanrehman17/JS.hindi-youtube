a65d1202a87e0772829cab6682deec121c67fc01
rehanrehman17 <rehanrehman060@gmail.com>
2025-10-17 19:35:59 +0000

    03_conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,40 @@
+let score = "33abc"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// jb humm sb ko number me convert krty hain tw kya hta for example
+// "33" => 33 
+// "33abc" => NaN (not a number)
+// undefined => NaN
+// true or false => 1 , 0
+// null => 0
+
+let isLoggedIn = 1
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanisLoggedIn);
+
+// 1 => true and 0 => false
+// "" => false
+// null => false
+// undefined => false 
+// rehan => true
+
+let someNumber = "44"
+
+let StringNumber = String(someNumber)
+
+console.log(typeof StringNumber);
+console.log(StringNumber);
+
+
+
+
+
