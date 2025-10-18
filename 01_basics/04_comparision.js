169f97e7ba0c845d2ea900763294474f9ba7c7c3
rehanrehman17 <rehanrehman060@gmail.com>
2025-10-18 18:58:06 +0000

    04_comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,42 @@
+// these all are common and simple comparision
+//console.log(2 > 1);
+//console.log(2 < 1);
+//console.log(2 >= 1);
+//console.log(2 = 2);
+//console.log(2 != 2);
+
+
+//JavaScript me type conversion (automatic type change) tab hoti hai jab 2 alag type ke values compare ki jati hain, jaise:
+//String vs Number
+//Boolean vs Number
+//Null / Undefined vs Number
+
+//console.log("2" > 1);
+//console.log(1 < "2");
+//console.log("2" > "3");
+
+console.log(null > 0);
+console.log(null == 0); // JavaScript does not convert null to a number when using ==.
+//*************************It only does that in numeric comparisons (>, <, >=, <=).
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined > 0);
+// es trah k conversion humee kbhi bhi confusion me daal skty hain to humm es trah k comparision ko avoid he krty hain 
+
+// ===
+
+console.log("2" === 1); // esme hmare pass kya hta hai k === stickly check krta hai sb kch types wgera k humm conversion nh kr skty q k yahn === hai or enka datatype bh chng hai
+
+
+
+
+
+
+
+
+
+
+
+
