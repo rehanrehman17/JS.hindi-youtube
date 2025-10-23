fc9249b5db5738d570c435c1cddd75dfad18ffaf
rehanrehman17 <rehanrehman060@gmail.com>
2025-10-23 18:28:04 +0000

    06_nums_and_maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,50 @@
+const score = 400
+//console.log(score);
+
+const balance = new Number(100)
+//console.log(balance);
+
+//console.log(typeof String (balance.toString().length));
+//console.log(balance.toFixed(2));
+
+const newNumber = 250.4689
+//console.log(newNumber.toPrecision(3));
+
+const otherNumber = 1000000
+//console.log(otherNumber.toLocaleString(en-IN));
+
+// ++++++++++++++++++++++ Maths +++++++++++++++++++++++
+
+//console.log(Math);
+//console.log(Math.abs(-4));
+//console.log(Math.round(4.6));
+//console.log(Math.ceil(4.3));
+//console.log(Math.floor(4.9));
+// console.log(Math.pow(4, 2)); output: 2**2
+
+
+console.log(Math.random()); // hamesha values choose krta hai between (0 to 1)
+console.log((Math.random()*10) +1 ); // essy mjhe values 1 to 9 k beech milyy gii 1 add krne se 
+console.log(Math.floor((Math.random() *10) +1 ));
+
+const Min = 10
+const Max = 20
+
+console.log(Math.floor(Math.random() * (Max - Min + 1)) + Min);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
