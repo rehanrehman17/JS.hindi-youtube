6a3cc3d3748088eeec810ccb4a691b3d77f71d39
rehanrehman17 <rehanrehman060@gmail.com>
2025-11-04 12:06:52 +0000

    06_nums_and_maths

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -18,8 +18,8 @@ const otherNumber = 1000000
 //console.log(Math);
 //console.log(Math.abs(-4));
 //console.log(Math.round(4.6));
-//console.log(Math.ceil(4.3));
-//console.log(Math.floor(4.9));
+console.log(Math.ceil(4.3));
+console.log(Math.floor(4.9));
 // console.log(Math.pow(4, 2)); output: 2**2
 
 
